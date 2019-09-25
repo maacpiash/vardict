74ba7dfb67430327c4b0ffb54a28b8452e247eac
Ahad Chowdhury <ahadc@maacpiash.com>
2019-09-25 19:38:36 +0600

    Add test
    This test is a bit unusual. Instead of using a conventional test library
    (e.g. Jest), I used a normal TS program that would be called with some
    certain CLI args — something I could not have done with Jest.

---
+++ vardict.test.ts
@@ -0,0 +1,5 @@
+import vardict from './vardict';
+
+const { name, age, single, c, home, coolWithLadies } = vardict;
+
+((name === 'Anirudha') && (age === 25) && (single) && (c === 'Samsung R&D') && (home === 'Mymensingh') && (!coolWithLadies) && process.exit(0)) || process.exit(1);
